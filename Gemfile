63e15313df037605e544783eaddd853713dd1ed4
umtrey <dfsiii@gmail.com>
2014-02-24 00:50:57 -0600

    let's get started

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.3'
+gem 'pg'
+gem 'thin'
+gem 'turbolinks'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+group :assets do
+  gem 'sass-rails', '~> 4.0.0'
+  gem 'uglifier', '>= 1.3.0'
+  gem 'coffee-rails', '~> 4.0.0'
+  gem 'therubyracer', platforms: :ruby
+  gem 'jquery-rails'
+end
+
+group :development do
+  gem 'pry'
+end
+
+group :test do
+  gem 'rspec-rails'
+  gem 'cucumber-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tritracker2::Application.config.secret_key_base = 'bce0f87b79ae31b554abbc7a872852d2c6a07144d71f67577b612ea8a88f98b976526d35a079172885994186bda531f80efbdf4e49896b3aa0b987004c9dba34'
